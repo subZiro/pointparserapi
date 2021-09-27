5e026e0a51a547f60a90836c95704c17ee358cc0
Ilya <49365758+subZiro@users.noreply.github.com>
2021-09-27 15:41:20 +0400

    контролеры поиска точек

---
+++ points_parser_api/pointparser/app/api_v1/endpoints/parse/__init__.py
@@ -0,0 +1,4 @@
+from .add_point import AddPointAdmCrud
+from .parse_points import ParsePointsAdmCrud
+from .refresh_base_points import RefreshPointsAdmCrud
+from .run_normalize import RunNormalizeAdmCrud
