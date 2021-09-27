8d90a5642edc21f1a2b16f7b67fb949e16a18465
Ilya <49365758+subZiro@users.noreply.github.com>
2021-09-27 16:29:06 +0400

    исправил описание разделов

--- points_parser_api/pointparser/app/api_v1/namespaces.py
+++ points_parser_api/pointparser/app/api_v1/namespaces.py
@@ -6,9 +6,9 @@ from flask_restplus import Namespace
 
 ns2 = Namespace('MainPage', description='Приветствие апи', path='/')
 ns3 = Namespace('FindPoints', description='Поиск новых точек', path='/parse')
-ns4 = Namespace('Points', description='Поиск новых точек', path='/points')
+ns4 = Namespace('Points', description='Точки хранимые в БД', path='/points')
 ns5 = Namespace('Map', description='Вывод информации на карту', path='/map')
-ns6 = Namespace('Users', description='Вывод информации на карту', path='/')
+ns6 = Namespace('Users', description='Аккаунты', path='/')
 ns7 = Namespace('CeleryMonitor', description='Работа с заданиями', path='/task')
 
 namespaces_tuple = (ns2, ns3, ns4, ns5, ns6, ns7)
